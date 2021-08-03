4cc0f3635e497dcaddd18395b7d54a1809371eaa
student <student@K09-09>
2021-08-03 17:54:38 +0300

    Добавление модулей

--- Parent/MainWindow.xaml.cs
+++ Parent/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Student
+namespace Parent
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
